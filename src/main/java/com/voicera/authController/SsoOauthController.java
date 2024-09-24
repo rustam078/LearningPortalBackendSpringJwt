2d3df133711243267924445ea0f7ab0a1be8f753
Rustam+1-at-963782499227 <Rustam+1-at-963782499227>
2024-09-24 12:49:16 +0530

    minor bugs api fix

--- src/main/java/com/voicera/authController/SsoOauthController.java
+++ src/main/java/com/voicera/authController/SsoOauthController.java
@@ -1,9 +1,6 @@
 package com.voicera.authController;
 
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -11,18 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.voicera.config.JwtService;
-import com.voicera.repository.UserRepository;
 import com.voicera.token.Token;
 import com.voicera.token.TokenRepository;
-import com.voicera.token.TokenType;
-import com.voicera.user.Role;
 import com.voicera.user.User;
 
 @RestController
@@ -30,54 +22,30 @@ public class SsoOauthController {
     private static final Logger logger = LoggerFactory.getLogger(SsoOauthController.class);
 
     @Autowired
-    private JwtService jwtService;
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
 	private  TokenRepository tokenRepository;
     @Autowired
     @Lazy
     private PasswordEncoder encoder;
 
 
-@GetMapping("/userdtls/{email}")
-public ResponseEntity<?> userToken(@PathVariable String email) {
-	System.out.println("SsoOauthController.userToken() "+email);
-	   var user = userRepository.findByEmail(email).orElseThrow(() -> {
-           logger.error("User not found for email: {}", email);
-           return new UsernameNotFoundException("User not found");
-       });
-       
-       var jwtToken = jwtService.generateToken(user);
-       revokeAllUserTokens(user);
-       saveUserToken(user, jwtToken);
+@GetMapping("/userdtls/{token}")
+public ResponseEntity<?> userToken(@PathVariable String token) {
+	System.out.println("SsoOauthController.userToken() "+token);
+	   var user = tokenRepository.findByToken(token);
+      if(!user.isPresent()) {
+    	  throw new RuntimeException("Invalid Credentials...");
+      }
+    	  Token tokendtls = user.get();
+    	  if(tokendtls.isExpired()||tokendtls.isRevoked()) {
+    		  throw new RuntimeException("Token  invaild..."); 
+    	  }
+    	  User userdtls = tokendtls.getUser();
        AuthenticationResponse autresp = new AuthenticationResponse();	
-       BeanUtils.copyProperties(user, autresp);
-       autresp.setToken(jwtToken);
-       
-       Set<Role> rolesSet = new HashSet<>();
-       rolesSet.add(user.getRole());
-       autresp.setRoles(rolesSet);
-
-       logger.info("Returning authentication response for user: {}", email);
+       BeanUtils.copyProperties(userdtls, autresp);
+       autresp.setToken(token);
+       logger.info("Returning authentication response for user: {}", user);
        return new ResponseEntity<>(autresp, HttpStatus.OK);
+       
 }
 
-
-	private void saveUserToken(User user, String jwtToken) {
-		var token = Token.builder().user(user).token(jwtToken).tokenType(TokenType.BEARER).expired(false).revoked(false)
-				.build();
-		tokenRepository.save(token);
-	}
-
-	private void revokeAllUserTokens(User user) {
-		var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
-		if (validUserTokens.isEmpty())
-			return;
-		validUserTokens.forEach(token -> {
-			token.setExpired(true);
-			token.setRevoked(true);
-		});
-		tokenRepository.saveAll(validUserTokens);
-	}
 }
--- src/main/java/com/voicera/service/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/voicera/service/CustomOAuth2SuccessHandler.java
@@ -7,11 +7,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import com.voicera.config.JwtService;
+import com.voicera.repository.UserRepository;
+import com.voicera.token.Token;
+import com.voicera.token.TokenRepository;
+import com.voicera.token.TokenType;
+import com.voicera.user.User;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,9 +33,16 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
     @Value("${ui.domain.path}")
     private String uiPath;
     @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TokenRepository tokenRepository;
+    @Autowired
     private PasswordEncoder passwordEncoder;
     @Autowired
-    private PasswordGeneratorService passwordGeneratorService;
+    private PasswordGeneratorService passwordGeneratorService;    
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         logger.info("OAuth2 authentication successful");
@@ -37,11 +52,36 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
             OAuth2User oAuth2User = oauthToken.getPrincipal();
             String email = oAuth2User.getAttribute("email");
             logger.info("Authenticated user email: {}", email);
+            var user = userRepository.findByEmail(email).orElseThrow(() -> {
+                logger.error("User not found for email: {}", email);
+                return new UsernameNotFoundException("User not found unable to authenticate");
+            });
+            
+            var jwtToken = jwtService.generateToken(user);
+            revokeAllUserTokens(user);
+            saveUserToken(user, jwtToken);
             String password = passwordGeneratorService.generatePassword(20);
             String encodeurl = passwordEncoder.encode(password);
-            String redirectUrl = uiPath+"/oauth2/callback?clentId= "+encodeurl.concat(encodeurl)+" &code=" + email;
+            String redirectUrl = uiPath+"/oauth2/callback?clentId= "+encodeurl.concat(encodeurl)+" &code=" + jwtToken;
             response.sendRedirect(redirectUrl);
         }
     }
+    
+	private void saveUserToken(User user, String jwtToken) {
+		var token = Token.builder().user(user).token(jwtToken).tokenType(TokenType.BEARER).expired(false).revoked(false)
+				.build();
+		tokenRepository.save(token);
+	}
+
+	private void revokeAllUserTokens(User user) {
+		var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+		if (validUserTokens.isEmpty())
+			return;
+		validUserTokens.forEach(token -> {
+			token.setExpired(true);
+			token.setRevoked(true);
+		});
+		tokenRepository.saveAll(validUserTokens);
+	}
     
 }
